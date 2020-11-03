e2338fccac2a1f73f9b8cfe00b34b6d7b4d50ecc
dongwoo <dongou705@naver.com>
2020-11-03 23:19:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/binggo01/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.example.binggo01;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+    private FirebaseAuth mAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.loginButton).setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.loginButton:
+                    signUp();
+                    break;
+            }
+        }
+    };
+
+
+
+    // 회원가입 할때 함수를 따로 만들어 저장한다.
+    private void signUp() {
+        String email = ((EditText) findViewById(R.id.emailEditText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordEditText)).getText().toString();
+
+        //이메일의 길이가 0보다 클때만 사용할 수 있게해주는 if문이다.
+        if (email.length() > 0 && password.length() > 0) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startToast("로그인에 성공하였습니다.");
+                                startMainActivity();
+                            } else {
+                                //null이 아니라면 실행되게 if문
+                                if (task.getException() != null) {
+                                    startToast(task.getException().toString());
+                                }
+                            }
+
+                            // ...
+                        }
+                    });
+        } else {
+            startToast("이메일 또는 비밀번호를 입력해주세요.");
+        }
+    }
+    // 토스트를 읽지 못하는 경우가 생기기 때문에 StarToast로 따로 메소드를 만들어 addonCompleteListener에 if문
+    // 과 else문에 넣어준다.
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/binggo01/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.binggo01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            startSignUpActivity();
+        }
+        //로그아웃 기능 findViewById
+        findViewById(R.id.loginButton).setOnClickListener(onClickListener);
+    }
+    //로그아웃 기능 클릭시 사인업 액티비티로 이동
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.loginButton:
+                    FirebaseAuth.getInstance().signOut();
+                    startSignUpActivity();
+                    break;
+            }
+        }
+    };
+
+    private void startSignUpActivity() {
+        Intent intent = new Intent(this,SignUpActivity.class);
+        startActivity(intent);
+    }
+}
